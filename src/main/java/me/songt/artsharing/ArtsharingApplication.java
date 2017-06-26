fb68588449024282adcc556578ad52ecabe1badf
PvtTony <yusongtao518@outlook.com>
2017-06-27 00:32:38 +0800

    Initial Files Commit

---
+++ src/main/java/me/songt/artsharing/ArtsharingApplication.java
@@ -0,0 +1,14 @@
+package me.songt.artsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtsharingApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ArtsharingApplication.class, args);
+    }
+}
